ca0789df135e76c3b7a2a2f4f8db0cbf661b5972
Valeria <valeria.stamenova3@gmail.com>
2021-06-11 12:16:44 +0300

    Quadratic equations and greeting

---
+++ Tasks/Greeting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Student's name: ");
+            string studentName = Console.ReadLine();
+            Console.Write("Course: ");
+            string courseName = Console.ReadLine();
+
+            string greeting = $"Здравейте {studentName}, и добре дошли в {courseName}!";
+            Console.WriteLine(Environment.NewLine + greeting);
+        }
+    }
+}
---
+++ Tasks/QuadraticEquation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ax^2 + bx + c = 0");
+            
+            Console.Write("a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            double d = Math.Sqrt(Math.Pow(b, 2) - (4 * a * c));
+            double x1 = (-b + d) / (2 * a);
+            double x2 = (-b - d) / (2 * a);
+
+            Console.WriteLine($"Roots: x1 = {x1}, x2 = {x2}");
+        }
+    }
+}
