06194432c02e5a799c46baf53c0a5b2a1b7169a2
Valeria <valeria.stamenova3@gmail.com>
2021-06-12 21:33:01 +0300

    Added Factorial project

---
+++ Tasks/Factorial/FactorialForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    public partial class FactorialForm : Form
+    {
+        int number;
+
+        public FactorialForm()
+        {
+            InitializeComponent();
+        }
+
+        private void selectNumber_ValueChanged(object sender, EventArgs e)
+        {
+            number = (int)Math.Round(selectNumber.Value, 0);
+        }
+
+        private void iterationButton_Click(object sender, EventArgs e)
+        {
+            ulong factorial = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= (ulong)i;
+            }
+
+            label1.Text = $"{number}! = {factorial}";
+        }
+
+        private void recursionButton_Click(object sender, EventArgs e)
+        {
+            ulong factorial = GetFactorialRecursively(number);
+
+            label1.Text = $"{number}! = {factorial}";
+        }
+
+        private ulong GetFactorialRecursively(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return (ulong)number * GetFactorialRecursively(number - 1);
+            }
+        }
+    }
+}
---
+++ Tasks/Factorial/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, do you want to open form? Y/N");
+
+            if(Console.ReadLine() == "Y")
+            {
+                Application.EnableVisualStyles();
+                Application.Run(new FactorialForm());
+            }
+            else
+            {
+                string toContinue = "Y";
+                while(toContinue == "Y")
+                {
+                    int number;
+                    Console.WriteLine("Please insert the number of which you want the factorial: ");
+
+                    bool isItValid = int.TryParse(Console.ReadLine(), out number);
+                    if (isItValid && number > 0)
+                    {
+                        Console.WriteLine("Do you want it by iteration or recursion? I/R");
+                        if(Console.ReadLine() == "I")
+                        {
+                            Console.WriteLine(GetFactorialIteratively(number));
+                        }
+                        else
+                        {
+                            Console.WriteLine(GetFactorialRecursively(number));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please insert valid number");
+                    }
+
+                    Console.WriteLine("Do you wish to continue? Y/N");
+                    toContinue = Console.ReadLine();
+                }
+            }
+        }
+
+        static ulong GetFactorialIteratively(int number)
+        {
+            ulong factorial = 1;
+            for(int i = 2; i <= number; i++)
+            {
+                factorial *= (ulong)i;
+            }
+
+            return factorial;
+        }
+
+        static ulong GetFactorialRecursively(int number)
+        {
+            if(number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return (ulong)number * GetFactorialRecursively(number - 1);
+            }
+        }
+    }
+}
