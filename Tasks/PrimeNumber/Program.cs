8b68af249cbca53deb07582708dd7636e71b6da7
Valeria <valeria.stamenova3@gmail.com>
2021-06-12 16:20:43 +0300

    Added PrimeNumbers project

---
+++ Tasks/PrimeNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (IsItPrime(number))
+            {
+                Console.WriteLine($"The number {number} is prime");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is NOT prime");
+            }
+        }
+
+        static bool IsItPrime(int number)
+        {
+            if(number < 0)
+            {
+                return false;
+            }
+
+            for(int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if(number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
