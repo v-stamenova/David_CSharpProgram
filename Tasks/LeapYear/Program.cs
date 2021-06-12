9082fb63154ebaa1aa3a946759a09a636ed3d0ca
Valeria <valeria.stamenova3@gmail.com>
2021-06-12 15:51:28 +0300

    Adding project LeapYear

---
+++ Tasks/LeapYear/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a year");
+            int year = int.Parse(Console.ReadLine());
+
+            if (IsTheYearLeap(year))
+            {
+                Console.WriteLine("The year is leap");
+            }
+            else
+            {
+                Console.WriteLine("The year is NOT leap");
+            }
+        }
+
+        static bool IsTheYearLeap(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
