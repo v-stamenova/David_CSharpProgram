cdc06d1a5e3a2fdcc22efd8fc1f5f7a7b6aafe32
Valeria <valeria.stamenova3@gmail.com>
2021-06-13 11:41:06 +0300

    Added FibonacciNumbers

---
+++ Tasks/FibonacciNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a number to find the Fibonacci number on that position:");
+            int number;
+            bool isItValid = int.TryParse(Console.ReadLine(), out number);
+            if (isItValid && number > 0)
+            {
+                Console.WriteLine($"Fibonacci number #{number}: {Fibonacci(number)}");
+            }
+            else
+            {
+                Console.WriteLine($"The number is not valid");  
+            }
+        }
+
+        static int Fibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return Fibonacci(number - 1) + Fibonacci(number - 2);
+            }
+        }
+    }
+}
