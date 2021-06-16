701de6a65652ef46cdd4ae3c1e09c655dfeb998c
Valeria <valeria.stamenova3@gmail.com>
2021-06-16 19:42:13 +0300

    Added TermGrades

---
+++ Tasks/TermGrades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            Console.WriteLine("Insert students and their grades for example -> Michael: 5 6 3,5");
+            Console.WriteLine("If you want to stop inserting students insert END");
+
+            List<string> readLine = Console.ReadLine().Split(' ').ToList();
+            while(readLine[0] != "END")
+            {
+                Student student = new Student(readLine[0]);
+                readLine.RemoveAt(0);
+                AddGrades(student, readLine.Select(double.Parse).ToList());
+                students.Add(student);
+                readLine = Console.ReadLine().Split(' ').ToList();
+            }
+
+            Console.WriteLine(Environment.NewLine + "-----------" + Environment.NewLine);
+
+            double sumOfAverageGrades = 0;
+            foreach(Student student in students)
+            {
+                Console.WriteLine($"{student.Name} - {Math.Round(student.AverageGrade(), 2)}");
+                sumOfAverageGrades += student.AverageGrade();
+            }
+
+            Console.WriteLine($"Average class grade: {Math.Round((sumOfAverageGrades / students.Count), 2)}");
+        }
+
+        static void AddGrades(Student student, List<double> grades)
+        {
+            grades.RemoveAll(x => x < 2 || x > 6);
+            student.Grades = grades;
+        }
+    }
+}
---
+++ Tasks/TermGrades/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermGrades
+{
+    public class Student
+    {
+        private string name;
+        private List<double> grades;
+        
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                this.name = value;
+            }
+        }
+
+        public List<double> Grades
+        {
+            get
+            {
+                return this.grades;
+            }
+            set
+            {
+                this.grades = value;
+            }
+        }
+
+        public Student(string name)
+        {
+            this.Name = name;
+            this.Grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            this.grades.Add(grade);
+        }
+
+        public double AverageGrade()
+        {
+            return this.grades.Average();
+        }
+    }
+}
