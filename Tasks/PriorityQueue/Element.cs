dffdc5ec035fd5d228d961124894c81282a048c5
Valeria <valeria.stamenova3@gmail.com>
2021-07-05 11:51:29 +0300

    Add PriorityQueue

---
+++ Tasks/PriorityQueue/Element.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+	class Element<T>
+	{
+		private T value;
+		private int priority;
+
+		public T Value { get => this.value; set => this.value = value; }
+
+		public int Priority { get => this.priority; set => this.priority = value; }
+
+		public Element(T value, int priority)
+		{
+			this.value = value;
+			this.priority = priority;
+		}
+
+		public override string ToString()
+		{
+			return $"{this.value} - {this.priority}";
+		}
+	}
+}
---
+++ Tasks/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriorityQueue
+{
+	class PriorityQueue<T> : IEnumerable<Element<T>>
+	{
+		private List<Element<T>> queue = new List<Element<T>>();
+
+		public void AddElement(Element<T> element)
+		{
+			int i = 0;
+			i = queue.IndexOf(queue.Find(x => x.Priority < element.Priority));
+			if(i == -1)
+			{
+				queue.Add(element);
+			}
+			else
+			{
+				queue.Insert(i, element);
+			}
+		}
+
+		public string Peek()
+		{
+			return queue[0].ToString();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable)queue).GetEnumerator();
+		}
+
+		public IEnumerator<Element<T>> GetEnumerator()
+		{
+			return ((IEnumerable<Element<T>>)queue).GetEnumerator();
+		}
+	}
+}
